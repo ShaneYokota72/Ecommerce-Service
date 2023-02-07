3bd006538c6d129546adf85c836b5ae5efd33991
Shane Yokota <shaneyok@usc.edu>
2023-02-07 14:42:33 -0800

    mydatasore debugge

--- mydatastore.cpp
+++ mydatastore.cpp
@@ -1,4 +1,6 @@
 #include "mydatastore.h"
+#include "util.h"
+#include <set>
 
 using namespace std;
 
@@ -13,21 +15,58 @@ void MyDataStore::addUser(User* u){
     uservector.push_back(u);
 }
 std::vector<Product*> MyDataStore::search(std::vector<std::string>& terms, int type){
-    vector<Product*> result;
-    if(type = 0){
+    /* vector<Product*> result; */
+    set<Product*> resultset;
+    if(type == 0){
         //intersection
-        result = setIntersection(productvector, terms);
+        for(int i=0; i<productvector.size(); i++){
+            set<string> productword = parseStringToWords((*productvector.at(i)).getName());
+            bool match = true;            
+            for(int j=0; j<terms.size(); j++){
+                set<string>::iterator it = productword.find(terms.at(i));
+                if(it == productword.end()){
+                    match = false;
+                }/* 
+                set<string> match = setIntersection<string>(parseStringToWords((*productvector.at(i)).getName()), parseStringToWords(terms.at(i)));
+                if(match.size()>0){
+                    resultset.insert(productvector.at(i));
+                } */
+            }
+            if(match){
+                resultset.insert(productvector.at(i));
+            }
+        }
+        /* resultset = setIntersection<Product*>(productvector, terms);
+        vector<Product*> result(resultset.size());
+        std::copy(resultset.begin(), resultset.end(), result.begin()); */
     } else {
         // union
-        result = setUnion(productvector, terms);
+        for(int i=0; i<productvector.size(); i++){
+            set<string> productword = parseStringToWords((*productvector.at(i)).getName());
+            bool match = false;            
+            for(int j=0; j<terms.size(); j++){
+                set<string>::iterator it = productword.find(terms.at(i));
+                if(it != productword.end()){
+                    match = true;
+                }/* 
+                set<string> match = setIntersection<string>(parseStringToWords((*productvector.at(i)).getName()), parseStringToWords(terms.at(i)));
+                if(match.size()>0){
+                    resultset.insert(productvector.at(i));
+                } */
+            }
+            if(match){
+                resultset.insert(productvector.at(i));
+            }
+        }
     }
-    return result;
+    vector<Product*> ans(resultset.begin(), resultset.end());
+    return ans;
 }
 void MyDataStore::dump(std::ostream& ofile){
-    for(int i=0; i<productvector.length(); i++){
-        (*productvector.at(i)).dump();
+    for(int i=0; i<productvector.size(); i++){
+        (productvector.at(i))->dump(ofile);
     }
-    for(int i=0; i<uservector.length(); i++){
-        (*uservector.at(i)).dump();
+    for(int i=0; i<uservector.size(); i++){
+        (uservector.at(i))->dump(ofile);
     }
 }
